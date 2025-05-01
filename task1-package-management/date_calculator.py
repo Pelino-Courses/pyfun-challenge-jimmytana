17502706ee6d676a6d2c6a9ceb2f1df532985304
jimmytana <kubwimanajimmy96@gmail.com>
2025-05-01 15:40:08 +0200

    first init

---
+++ task1-package-management/date_calculator.py
@@ -0,0 +1,9 @@
+import datetime
+def diference(date1,date2):
+    datediff=date2-date1
+    return datediff.days
+date1=input("please enter date 1:")
+date2=input("pleasea enter date 2:")
+date1=datetime.datetime.strptime(date1,"%Y-%m-%d").date()
+date2=datetime.datetime.strptime(date2,"%Y-%m-%d").date()
+print(diference(date1,date2))
