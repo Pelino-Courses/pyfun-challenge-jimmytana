c4b591caa62c429199d47ab755eb21800ac9319e
jimmytana <kubwimanajimmy96@gmail.com>
2025-05-02 16:14:16 +0200

    resolving and handling calculator errors

--- task3-arbitrary-arguments/calculator.py
+++ task3-arbitrary-arguments/calculator.py
@@ -3,10 +3,10 @@ calculator.py
 simple calculator that add,divide,substract,mulitply using *args,and **kwags.
 
 """
-def ad(*args):
+def add(*args):
     return sum(args)
 def substract(*args):
-    if len(args)< 2:
+    if len(args) < 2:
         raise ValueError("substraction needs two numbers.")
     result=args[0]
     for num in args[1:]:
@@ -33,7 +33,7 @@ def calculate(*args,**kwargs):
     **kwargs
     we need to add ,substract,divide and multiply
     """
-    if not isinstance(arg,(int,float)):
+    if not all(isinstance(arg,int) for arg in args):
         raise TypeError("all arguments shuld be numbers.")
     operation=kwargs.get('operation')
     if operation =='add':
@@ -46,19 +46,23 @@ def calculate(*args,**kwargs):
         return divide(*args) 
     else :
         raise ValueError("invalid operation")
-    if __name__=="__main__":
-        test_cases=[(10,5,2),(100,10),(8,2,0),('a',2)]
-        operations=['add','substract','multiply','divide']
-        for op in operations:
-            print("\noperation;{op}")
-            for case in  operations:
-                try :
-                    result=calculate(*case,operation=op)
-                    print(f"calculate{case},operation='{op}=>{result}")
-                except Exception as e:
-                    print(F"Error with input{case}:{e}") 
-                    
-        
+    
+    
+# print(calculate(3,4,5, operation="add"))
+    
+if __name__=="__main__":
+    test_cases=[(10,5,2),(100,10),(8,2,0),('a',2)]
+    operations=['add','substract','multiply','divide']
+    for op in operations:
+        print("\noperation: {op}")
+        for case in  test_cases:
+            try :
+                result=calculate(*case,operation=op)
+                print(f"calculate{case},operation='{op}=>{result}")
+            except Exception as e:
+                print(F"Error with input{case}:{e}") 
+                
+    
     
      
         
