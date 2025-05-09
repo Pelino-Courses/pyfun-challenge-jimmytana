64f9304b6121f19af31d2c0738241e412c29376c
jimmytana <kubwimanajimmy96@gmail.com>
2025-05-09 18:26:41 +0200

    teaching assistance details

--- task6-advanced-oop/teaching_assistant.py
+++ task6-advanced-oop/teaching_assistant.py
@@ -1,5 +1,8 @@
 from instructor import Instructor
 from student import Student 
+"""
+we need to import instructor and student as teaching assistance are being consided as student too
+"""
 class Teaching_Assistant(Instructor, Student):
     def __init__(self,ta_id, ta_name, ta_age, ta_sex, ta_course):
         Student.__init__(ta_id, ta_name, ta_age, ta_sex)
